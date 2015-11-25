1d6bf2facad4555653ce470bcc8aa8d03a9a3bcc
Xingchen Hong <hello@xc-h.net>
2015-11-25 10:30:56 -0600

    Bump version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodiase:infinite-load',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'A helper library for loading items from a collection incrementally.',
   // URL to the Git repository containing the source code for this package.
